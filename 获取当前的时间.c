651a7e98e7a9d6446a1e2b6aaed5d5d9f1dcb404
AuraSF <370402166@qq.com>
2020-11-22 23:43:35 +0800

    Update 获取当前的时间.c

--- 获取当前的时间.c
+++ 获取当前的时间.c
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <time.h>
+#include <math.h>
 
 
 time_t nowsec;      // time_t本质是整形，目的是存储time()获得的秒数
@@ -34,6 +35,23 @@ int main(int argc, char *argv[])
 
     printf( " * 现在时间 : %02d:%02d:%02d \n", nowhour, nowmin, nowsec);
     printf( " * 今天日期 : %04d年%02d月%02d日 \n", 1900+pnow->tm_year, 1+ pnow->tm_mon, pnow->tm_mday);  // 因为tm结构体从1900年算，所以年份要加上1900；因为月份是0-11，所以月份要+1
+    
+    
+    
+    
+    //  time库中函数的使用 
+    printf(" 上面使用的是手动提取时间结构体内的各个成员。库中的转化函数asctime()可直接将时间结构体转化为人类可读的字符串 \n");
+    
+    char * now = asctime(pnow);
+    printf(" * 现在的时间是 %s \n ", now);
+    
+    printf(" ctime()可以直接将秒数转化为人类可读的字符串 \n");
+    
+    
+    time(&nowsec);          // 这里不能使用之前的nowsec的值。 因为未知的原因，nowsec的值似乎被改写了，所以这里需要重新获取秒数 
+    now = ctime(&nowsec);
+    printf(" * 现在的时间是 %s \n ", now);
+
 
     getchar();
     return 0;
